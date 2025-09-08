300ca4cdb25f54b07bae022747ef6996154e6734
Gabrielle Baniqued <gabrielle.baniqued@bishopireton.org>
2025-09-08 10:51:06 -0400

    AllStudents.java

---
+++ Unit 1/AllStudents.java
@@ -0,0 +1,20 @@
+public class AllStudents {
+
+   public static void main(String[] args) {
+      
+   }
+   
+   public static void standForPrayer() {
+      System.out.println("Praying...");
+   }
+   
+   public static void uniformCheck() {
+      System.out.println("Our shirts are tucked in!");
+   }
+   
+   public static void beQuiet() {
+      System.out.println("...");
+   }
+   
+
+}
